828cf79c00a450770eff9a7209ef3b286f4df24e
MajedAlthonayan <majedalthonayan123@gmail.com>
2024-06-12 13:54:58 +0100

    Updating code to decode and encrypt the user transactions.

--- DecodingTX.sol
+++ DecodingTX.sol
@@ -1,148 +1,141 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-contract DecodingTX{
 
-    // Constants for RLP encoding prefixes
-    uint8 constant STRING_ASIS_START = 0x00; //0
-    uint8 constant STRING_SHORT_START = 0x80; //128
-    uint8 constant STRING_LONG_START = 0xb8; //184
-    uint8 constant LIST_SHORT_START = 0xc0; //192
-    uint8 constant LIST_LONG_START = 0xf8; //248
+import "solidity-rlp/contracts/RLPReader.sol";
+import "fhevm/lib/TFHE.sol";
 
-    struct DecodedTransaction {
-        bytes nonce;
-        bytes gasPrice;
-        bytes gasLimit;
-        bytes to;
-        bytes value;
-        bytes v;
-        bytes r;
-        bytes s;
+/**
+    * @dev This contract decodes raw, serialised Ethereum transactions. 
+    *
+    * This contract contains one primary function (decodeTX) in addition to three helper functions 
+    * to aid in decoding raw transaction into the nonce, gasPrice, gasLimit, to, value and data. 
+    *
+    */
+contract TXDecoder {
+    
+    using RLPReader for RLPReader.RLPItem;
+    using RLPReader for bytes;
+
+    struct Data {
+        bytes methodID;
+        euint64 amountIn;
+        euint64 amountOutMin;
+        euint64 addressOffset;
+        eaddress dataTo;
+        euint64 deadline;
+        euint64 addressLength;
+        eaddress token1;
+        eaddress token2;
+    }
+    struct DecodedTX {
+        euint64 nonce;
+        euint64 gasPrice;
+        euint64 gasLimit;
+        eaddress to;
+        euint64 value;
+        Data data;
     }
 
-    event debug(string message,uint256 x, uint256 y);
-
-    function decodeTX(bytes calldata rawTransaction) public{
-        DecodedTransaction memory userTX = decodeRawTransaction(rawTransaction);
-        // emit DecodedTransaction()
-    }
-
-    function decodeRawTransaction(bytes memory rawTransaction)  internal returns (DecodedTransaction memory){
-        bytes[] memory items = decode(rawTransaction);
-        require(items.length >= 8, "Transaction length is too short!");
-        return DecodedTransaction({
-            nonce: items[0],
-            gasPrice: items[1],
-            gasLimit: items[2],
-            to: items[3],
-            value: items[4],
-            v: items[5],
-            r: items[6],
-            s: items[7]
-        });
-
-    }
-
-    function decode(bytes memory userTX)  internal returns(bytes[] memory){
-        uint256 offset;
-        uint256 dataLen;
-
-        if(userTX.length == 0){
-            return new bytes[](0);
-        }
+    /**
+        * 
+        * @dev Takes the RLP encoded transactions and returns an encrypted 
+        * struct of the transaction parameters.
+        * 
+        * @param {encodedTX} the RLP encoded transaction.
+        * @return FHE Encrypted, RLP decoded transaction.
+        *
+        */
+    function decodeTX(bytes memory encodedTx) public pure returns(DecodedTX memory){ 
         
-        (offset, dataLen) = decodeLength(userTX);
-        emit debug("DEBUGGGG!!!!!!!", dataLen, offset);
+        euint64 nonce = TFHE.asEuint64(encodedTx.toRlpItem().toList()[0].toUint()); 
+        euint64 gasPrice = TFHE.asEuint64(encodedTx.toRlpItem().toList()[1].toUint()); 
+        euint64 gasLimit = TFHE.asEuint64(encodedTx.toRlpItem().toList()[2].toUint()); 
+        eaddress to = TFHE.asEaddress(encodedTx.toRlpItem().toList()[3].toAddress()); 
+        euint64 value = TFHE.asEuint64(encodedTx.toRlpItem().toList()[4].toUint()); 
+        Data memory decodedData;
         
+        // DATA
+        bytes memory data = encodedTx.toRlpItem().toList()[5].toBytes();
+        bytes memory methodID = extractBytes(data, 0, 4);
 
-        bytes[] memory items = new bytes[](dataLen);
-        for (uint256 i = 0; i < dataLen; i++) {
-            uint256 length;
-            (items[i], length) = decodeItem(userTX, offset);
-            offset += length;
+        if(uint8(methodID[0]) == 0x18){ 
+            //tokens for eth
+            decodedData = decodeTokensForEth(data);
+        }else if(uint8(methodID[0]) == 0x7f){
+            //eth for tokens
+            decodedData = decodeEthForTokens(data);
+        } else{
+            revert("Invalid Function");
         }
-        return items;
+
+        //Probably not needed !! 
+        // euint64 v = TFHE.asEuint64(encodedTx.toRlpItem().toList()[6].toUint()); 
+        // euint64 r = TFHE.asEuint64(encodedTx.toRlpItem().toList()[7].toBytes());
+        // euint64 s = TFHE.asEuint64(encodedTx.toRlpItem().toList()[8].toBytes());
+
+        return DecodedTX(nonce, gasPrice, gasLimit, to, value, decodedData);
     }
 
-    function decodeLength(bytes memory userTX) pure internal returns(uint256, uint256){
-        uint256 length = uint256(userTX.length);
-        uint8 prefix = uint8(userTX[0]);
-
-        require(length > 0, "Cannot get Length of Null item!");
-        require(prefix >= 0xc0, "Not a list");
-
-        if(prefix <= 0xf7 && length > prefix - 0xbf){
-            // short list
-            return(1,prefix - 0xc0);
-
-        }else if(prefix <= 0xff && length > prefix - 0xf7){
-            // long list
-            uint256 lenOfListLen = prefix - 0xf7;
-            uint256 listLen = toInteger(extractBytes(userTX, 1, lenOfListLen));
-            return(1 + lenOfListLen,listLen);
-
-        }else{
-            // IF not conforming to RLP standards
-            revert("ERROR!!!");
-        }
-    }
-
-    function decodeItem(bytes memory userTX, uint256 offset) internal returns (bytes memory, uint256){
-        uint8 prefix = uint8(userTX[offset]);
-        if (prefix <= 0x7f){
-            // decode as is (byte)
-            return (bytes(abi.encodePacked(userTX[offset])), 1);
-        } else if (prefix <= 0xb7) {
-            // TODO: ?? 
-            // decode short string
-            uint256 length = prefix - 0x80;
-            
-            emit debug("Before error causing!", length, userTX.length);
-            // Running around 200 time
-            // TODO: causing issues
-            return (extractBytes(userTX, offset+1, length), length + 1);
-
-        } else if (prefix <= 0xbf) {
-            // TODO: ??
-            // decoding long string
-            uint256 lengthLength = prefix - 0xb9;
-            uint256 length = toInteger(extractBytes(userTX, offset + 1, lengthLength));
-            return (_copy(userTX, offset + lengthLength + 1, length), length + lengthLength + 1);
-        } else if (prefix <= 0xf7) {
-            // decoding short list!
-            revert("Decoding lists not supported in this function");
-        } else {
-            revert("Invalid RLP item prefix");
-        }
-    }
-
+    /**
+        * 
+        * @dev Copies slices of an array into an new variable.
+        *
+        * @param {input} The initial array to be sliced. 
+        * @param {start} The starting index of the slice.
+        * @param {len} The length of the slice. 
+        * @return 
+        *
+        */
     function extractBytes(bytes memory input, uint256 start, uint256 len) internal pure returns (bytes memory) {
-        bytes memory result = new bytes(1);
+        require(start + len <= input.length, "Overflow!"); // Check bounds
+        bytes memory result = new bytes(len);
         for (uint256 i = 0; i < len; i++) {
             result[i] = input[start + i];
         }
         return result;
     }
 
-    function toInteger(bytes memory b) internal pure returns (uint256) {
-        uint256 length = b.length;
-        require(length > 0, "Input is null");
-
-        uint256 result = 0;
-        for (uint256 i = 0; i < length; i++) {
-            result = result * 256 + uint8(b[i]);
-        }
-        return result;
+    /**
+        * 
+        * @dev Decodes and encrypts the abi encoded input data field of the transaction 
+        * for the Tokens for Eth uniswapV2 functions.
+        *
+        * @param {data} the data field of the transaction.
+        * @return The data struct containing the parameters included in the data fields. 
+        *
+        */
+    function decodeTokensForEth(bytes memory data) internal pure returns(Data memory){
+        euint64 amountIn = TFHE.asEuint64(abi.decode(extractBytes(data, 4, 32), (uint256))); 
+        euint64 amountOutMin = TFHE.asEuint64(abi.decode(extractBytes(data, 36, 32), (uint256))); 
+        euint64 addressOffset = TFHE.asEuint64(abi.decode(extractBytes(data, 68, 32), (uint256)));
+        eaddress dataTo = TFHE.asEaddress(abi.decode(extractBytes(data, 100, 32), (address))); 
+        euint64 deadline = TFHE.asEuint64(abi.decode(extractBytes(data, 132, 32), (uint256))); 
+        euint64 addressLength = TFHE.asEuint64(abi.decode(extractBytes(data, 164, 32), (uint256)));
+        eaddress token1 = TFHE.asEaddress(abi.decode(extractBytes(data, 196, 32), (address))); 
+        eaddress token2 = TFHE.asEaddress(abi.decode(extractBytes(data, 228, 32), (address))); 
+        return Data(extractBytes(data, 0, 4),amountIn, amountOutMin, addressOffset, dataTo, deadline, addressLength, token1, token2);
     }
-    function _copy(bytes memory data, uint256 offset, uint256 length) pure internal returns (bytes memory) {
-        bytes memory result = new bytes(length);
-        for (uint256 i = 0; i < length; i++) {
-            result[i] = data[offset + i];
-        }
-        return result;
-    }
 
+    /**
+        * 
+        * @dev Decodes and encrypts the abi encoded input data field of the transaction 
+        * for the Eth for Tokens uniswapV2 functions.
+        *
+        * @param {data} the data field of the transaction.
+        * @return The data struct containing the parameters included in the data fields. 
+        *
+        */
+    function decodeEthForTokens(bytes memory data) internal pure returns(Data memory){
+        // self made
+        euint64 amountIn = TFHE.asEuint64(0);  
+        euint64 amountOutMin = TFHE.asEuint64(abi.decode(extractBytes(data, 4, 32), (uint256))); 
+        euint64 addressOffset = TFHE.asEuint64(abi.decode(extractBytes(data, 36, 32), (uint256)));
+        eaddress dataTo = TFHE.asEaddress(abi.decode(extractBytes(data, 68, 32), (address))); 
+        euint64 deadline = TFHE.asEuint64(abi.decode(extractBytes(data, 100, 32), (uint256)));
+        euint64 addressLength = TFHE.asEuint64(abi.decode(extractBytes(data, 132, 32), (uint256)));
+        eaddress token1 = TFHE.asEaddress(abi.decode(extractBytes(data, 164, 32), (address))); 
+        eaddress token2 = TFHE.asEaddress(abi.decode(extractBytes(data, 196, 32), (address))); 
+        return Data(extractBytes(data, 0, 4), amountIn, amountOutMin, addressOffset, dataTo, deadline, addressLength, token1, token2);
+    }
 }
-
-
